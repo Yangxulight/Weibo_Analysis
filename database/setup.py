2d2f6152d308b79e73169e084fdae089a962a02e
Ryan <825570036@qq.com>
2018-02-26 21:06:45 +0800

    Mock Login and save cookies to build request for data crawling

---
+++ database/setup.py
@@ -0,0 +1,5 @@
+import os
+
+PATH_TO_DATABASE = os.getcwd()
+PATH_TO_PROJECT = os.path.abspath(os.path.join(PATH_TO_DATABASE, os.pardir))
+COOKIE_FILE_NAME = "cookies.ini"
--- database/weibo_data.py
+++ database/weibo_data.py
@@ -1,6 +1,12 @@
 import re
+from database.cookie import Cookie
+import requests
 
 class WeiboData:
+    login_url = "https://weibo.cn"
+    base_url = "https://weibo.cn/u/{}"
+    username = "yangxulight@outlook.com"
+    password = "light3768"
     @classmethod
     def fetch_user_list(cls):
         pattern = re.compile('action-data"')
@@ -15,11 +21,26 @@ class WeiboData:
 
     @classmethod
     def fetch_user_weibo(cls, uid):
+        uid_list = WeiboData.fetch_user_list()
+        cookies = Cookie.load_cookies()
+        request = self.build_request(cookies)
+        for uid in uid_list:
+            page_url = base_url.format(uid)
+            response = request.get(page_url)
+            self.save_data(response.content)
+
+
+    def build_request(self, cookies):
+        request = requests.Session()
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36'}
+        request.headers.update(headers)
+        for cookie in cookies:
+            request.cookies.set(cookie['name',cookie['value']])
+        return request
+
+    def save_data(self, data):
         pass
 
-    @classmethod
-    def save_cookie(cls):
-        
 
         
         
